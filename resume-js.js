3165c7d9c3fb12a77a29bf215f357b8e3842ae07
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-06-03 18:11:50 -0500

    feat: made changes to website

--- resume-js.js
+++ resume-js.js
@@ -4,4 +4,9 @@
         alert("ready");
     });
 
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        e.target // newly activated tab
+        e.relatedTarget // previous active tab
+    });
+
 });
